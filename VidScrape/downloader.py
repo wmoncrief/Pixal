c07a66872ef8b2cc7b2b12177715f759b7291451
Wesley Moncrief <wesmoncrief@gmail.com>
2016-11-28 11:04:37 -0600

    downloads all videos into directory when you run this file

--- VidScrape/downloader.py
+++ VidScrape/downloader.py
@@ -1,22 +1,40 @@
-import pafy
+import pafy  # you should run 'pip install youtube-dl' to optimize pafy
 import pandas as pd
+import os
 
-def download(url):
-	video = pafy.new(url)
-	best = video.getbest()
-	best.resolution, best.extension
-	best.download(quiet=False)
 
-#def load_csv(filepath):
-	#df = pd.read_csv(filepath)
-	#print df
+def download_vid(url,dir):
+    try:
+        video = pafy.new(url)
+        streams = video.streams
+        min_res_stream = streams[0]  # we want the lowest resolution video to speed up analysis
+        for s in streams:
+            if s.resolution < min_res_stream.resolution:
+                min_res_stream = s
 
-fp = 'C:\Users\Chris\Desktop\Classes\CSCE489\BigBook\BigBook\VidScrape\country.csv'
-df = pd.read_csv(fp)
-print df
+        print min_res_stream.filename
+        min_res_stream.download(quiet=False,filepath=dir + '/' + min_res_stream.filename )  # def load_csv(filepath):
+    except BaseException as err:
+        print "failed to download a video: " + str(err)
 
-count = 0 #was looking for the int index but got lazy
-for i in df['URL']:
-	url = 'https://www.youtube.com' + df['URL'][count] 
-	download(url)
-	count = count + 1
+
+
+def download_csv(df,dir):
+    count = 0  # was looking for the int index but got lazy
+    for i in df['URL']:
+        url = 'https://www.youtube.com' + df['URL'][count]
+        download_vid(url,dir)
+        count = count + 1
+
+
+def download_all():
+    genres = ['country', 'edm', 'pop', 'rap', 'rock']
+    for genre in genres:
+        dir = genre + '_vids'
+        if not os.path.exists(dir):
+            os.makedirs(dir)
+
+        df = pd.read_csv(genre + '.csv')
+        download_csv(df,dir)
+
+download_all()
