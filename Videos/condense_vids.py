61bc0d348e7642f4303eba7813bc892ca8516a6d
Wesley Moncrief <wesmoncrief@gmail.com>
2016-12-01 00:02:00 -0600

    condense videos script. Run this in the same directory as your existing video directories!

---
+++ Videos/condense_vids.py
@@ -0,0 +1,23 @@
+import os
+from subprocess import call
+
+genres = ['country', 'edm', 'pop', 'rap', 'rock']
+
+#  run this in the same directory that holds all your video directories, so it should be
+#  a sibling of country_vids, edm_vids, etc.
+
+for genre in genres:
+    full_vids = os.listdir(genre + '_vids')
+    if not os.path.exists('compressed_' + genre):
+        os.makedirs('compressed_' + genre)
+    for old_vid in full_vids:
+        old_vid_path = genre + '_vids/' + old_vid
+        new_vid_path = 'compressed_' + genre + '/' + old_vid
+        print ' '.join(
+            ["ffmpeg", "-i", old_vid_path, "-s", "250x250", "-b:a", "200k", "-vcodec", "mpeg4", "-acodec", "copy",
+             new_vid_path])
+
+        call(["ffmpeg", "-i", old_vid_path, "-s", "250x250", "-b:a", "200k", "-vcodec", "mpeg4", "-acodec", "copy",
+              new_vid_path])
+
+print 'hi'
