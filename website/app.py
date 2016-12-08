5ec4edc61b486e82bda1fa5168d1f342cc4ed5b9
Clayton Petty <git.notyalc@gmail.com>
2016-12-07 23:42:09 -0600

    website changes

--- website/app.py
+++ website/app.py
@@ -1,7 +1,9 @@
 # Imports
-import os, time
+import os, time, path, pafy, pandas
+from PIL import Image, ImageDraw
+from analyzer import get_mode_colors
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
-import pandas
+import  gen_pic
 
 # create our little application :)
 app = Flask(__name__)
@@ -15,21 +17,53 @@ app.config.update(dict(
 ))
 
 # app.config.from_envvar('FLASK_SETTINGS', silent=True)
-
+def get_filename_extension(filename):
+    index = len(filename) - 1 - filename[::-1].index('.')
+    return filename[index:]
 
 def do_stuff():
-    time.sleep(5)
     return "yo"
+
+def download_vid(url,dir):
+    try:
+        video = pafy.new(url)
+        streams = video.streams
+        min_res_stream = streams[0]  # we want the lowest resolution video to speed up analysis
+        for s in streams:
+            if s.resolution < min_res_stream.resolution:
+                min_res_stream = s
+
+        print min_res_stream.filename
+        FILENAME = 'video' + get_filename_extension(min_res_stream.filename)
+        min_res_stream.download(quiet=False,filepath=dir + '/' + FILENAME)  # def load_csv(filepath):
+        return FILENAME
+        # FILENAME = min_res_stream.filename
+
+    except BaseException as err:
+        print "failed to download a video: " + str(err)
+
 
 @app.route('/', methods=['GET','POST'])
 def load_home():
+    # gen_pic.generate_image('videos/video.3gp', 'static/image.png')
     # return render_template('show_entries.html', entries=entries)
-
     if request.method == 'POST':
-        print "if ran"
-        print request.form['url']
-        print do_stuff()
-    return render_template('index.html')
+        genre = 'Determined Genre'
+        print 'Started Download!'
+        FILENAME = download_vid(request.form['url'], 'videos')
+        print 'Ended Download!'
+        print 'GENERATING IMAGE'
+        print 'GENERATING IMAGE'
+        print 'GENERATING IMAGE'
+        print 'GENERATING IMAGE'
+        print 'GENERATING IMAGE'
+        print 'GENERATING IMAGE'
+        video_path = os.path.join('videos', FILENAME).encode('ascii')
+        gen_pic.generate_image(video_path, 'static/image.png')
+        # gen_pic.generate_image('videos/video.3gp', 'static/image.png')
+        return render_template('index.html', is_post=True, genre=genre, show_pic=True)
+    else:
+        return render_template('index.html', is_post=False)
 
     # if not session.get('logged_in'):
         # abort(401)
---
+++ website/gen_pic.py
@@ -0,0 +1,32 @@
+from PIL import Image, ImageDraw
+from analyzer import get_mode_colors
+
+def generate_image(vid_path,im_path):
+    print 'Generate image:', vid_path, im_path
+    height = 40
+    width = 420
+    im = Image.new('RGB', (width, height))
+
+    color_list = get_mode_colors(vid_path)
+
+    colors = []
+    for i in range(0,len(color_list),3):
+        colors.append((color_list[i],color_list[i+1],color_list[i+2]))
+
+    draw = ImageDraw.Draw(im)
+
+    i = 10
+    width = 10 + i * 5
+    new_coord = [0,0,width,height]
+    for color in colors:
+        print new_coord
+        print width
+        width = 10 + i * 5
+        draw.rectangle(new_coord,color)
+        prev_coord = new_coord
+        new_coord = [prev_coord[2],0,prev_coord[2]+width,height]
+        i -= 1
+    # im.show()
+    im.save(im_path)
+
+
